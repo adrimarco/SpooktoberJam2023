80ea6a6adb95d07b6199a1c4f2fa39f11bd2273d
adrimarco <adrianmarco-@hotmail.es>
2023-10-03 19:10:20 +0200

    feat: player can run and adds lantern

--- Source/Spooktober2023/PlayerCharacter.cpp
+++ Source/Spooktober2023/PlayerCharacter.cpp
@@ -8,9 +8,16 @@
 #include "Components/CapsuleComponent.h"
 #include "Components/PointLightComponent.h"
 #include "Components/TimelineComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
-#define LIGHT_INTENSITY				5000.f
-#define LIGHT_ATTENUATION_RADIUS	2000.f
+
+constexpr auto LIGHT_INTENSITY			= 5000.f;
+constexpr auto LIGHT_ATTENUATION_RADIUS = 2000.f;
+constexpr auto MAX_STAMINA				= 10.f;
+constexpr auto MIN_RUN_STAMINA			= 5.f;
+constexpr auto STAMINA_RECOVER_RATE		= 0.8f;
+constexpr auto WALK_SPEED				= 400.f;
+constexpr auto RUN_SPEED				= 800.f;
 
 
 // Sets default values
@@ -18,6 +25,8 @@ APlayerCharacter::APlayerCharacter()
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	GetCharacterMovement()->MaxWalkSpeed = WALK_SPEED;
+	stamina = MAX_STAMINA;
 
 	// Capsule
 	GetCapsuleComponent()->InitCapsuleSize(40.f, 90.f);
@@ -32,8 +41,8 @@ APlayerCharacter::APlayerCharacter()
 	FPostProcessSettings postProcessSettings{};
 	postProcessSettings.bOverride_MotionBlurAmount = true;
 	postProcessSettings.bOverride_MotionBlurMax = true;
-	postProcessSettings.MotionBlurAmount = 0.5;
-	postProcessSettings.MotionBlurMax = 50;
+	postProcessSettings.MotionBlurAmount = 0.2;
+	postProcessSettings.MotionBlurMax = 20;
 
 	camera->PostProcessSettings = postProcessSettings;
 
@@ -46,7 +55,7 @@ APlayerCharacter::APlayerCharacter()
 	lampLight->SetupAttachment(lampMesh);
 	lampLight->SetAttenuationRadius(LIGHT_ATTENUATION_RADIUS);
 	lightIntensity = LIGHT_INTENSITY;
-	lampLight->SetIntensity(lightIntensity);
+	lampLight->SetIntensity(lightOn ? lightIntensity : 0.f);
 	lampLight->SetLightColor(FLinearColor(1.f, 0.173f, 0.f));
 
 	// Turn light on/off timeline
@@ -79,6 +88,7 @@ void APlayerCharacter::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 	ApplyCameraShake();
+	UpdateStamina(DeltaTime);
 }
 
 // Called to bind functionality to input
@@ -98,6 +108,12 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 		// Turn on/off light
 		if(LightAction)
 			EnhancedInputComponent->BindAction(LightAction, ETriggerEvent::Started, this, &APlayerCharacter::LightLamp);
+
+		// Running
+		if (RunAction) {
+			EnhancedInputComponent->BindAction(RunAction, ETriggerEvent::Started, this, &APlayerCharacter::StartSprint);
+			EnhancedInputComponent->BindAction(RunAction, ETriggerEvent::Completed, this, &APlayerCharacter::StopSprint);
+		}
 	}
 
 }
@@ -141,10 +157,42 @@ void APlayerCharacter::ApplyCameraShake() {
 	auto* playerController{ GetWorld()->GetFirstPlayerController() };
 	if (not playerController) return;
 
-	if (GetVelocity().Length() < 1.f) {
+	auto velocity = GetVelocity().Length();
+
+	if (velocity < WALK_SPEED) {
 		if (StaticShake) playerController->ClientStartCameraShake(StaticShake);
 	}
 	else {
-		if (MovementShake) playerController->ClientStartCameraShake(MovementShake);
+		if (MovementShake) playerController->ClientStartCameraShake(MovementShake, velocity/RUN_SPEED);
+	}
+}
+
+void APlayerCharacter::StartSprint(const FInputActionValue& Value = {}) {
+	if (exhausted || running) return;
+
+	running = true;
+	GetCharacterMovement()->MaxWalkSpeed = RUN_SPEED;
+}
+
+void APlayerCharacter::StopSprint(const FInputActionValue& Value = {}) {
+	running = false;
+	GetCharacterMovement()->MaxWalkSpeed = WALK_SPEED;
+}
+
+void APlayerCharacter::UpdateStamina(float time) {
+	if (running) {
+		// Loose stamina
+		stamina -= time;
+
+		if (stamina < 0.f) {
+			StopSprint();
+			exhausted = true;
+		}
+	}
+	else if (stamina < MAX_STAMINA){
+		// Recover stamina
+		stamina = FMath::Min(stamina + time * STAMINA_RECOVER_RATE, MAX_STAMINA);
+		
+		if (exhausted && stamina > MIN_RUN_STAMINA) exhausted = false;
 	}
 }
